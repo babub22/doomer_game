7664b53520bd51a985de27871560444c9476b5dc
babub22 <vladlesovoy2@gmail.com>
2024-02-10 18:33:21 +0100

    Add project files.

---
+++ deps.h
@@ -0,0 +1,14 @@
+#pragma once
+#pragma warning(disable:4996) // unsafe funcs like memcpy -> memcpy_s...
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+#include <stdint.h>
+#include <SDL2/SDL_image.h>
+#include <time.h>
+#include <float.h>
+#include <assert.h>
