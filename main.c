4f16cfc78c6e29bea1721e823e67510056e3bd02
babub22 <vladlesovoy2@gmail.com>
2024-02-14 14:57:44 +0100

    graphics: Make selection for walls

--- main.c
+++ main.c
@@ -4,8 +4,6 @@
 int main(int argc, char* argv[]) {
   SDL_Init(SDL_INIT_VIDEO);
 
-  const int windowW = 800;
-  const int windowH = 600;
 
   SDL_Window* window = SDL_CreateWindow("Doomer game",
 					SDL_WINDOWPOS_CENTERED,
@@ -18,35 +16,46 @@ int main(int argc, char* argv[]) {
   SDL_ShowCursor(SDL_DISABLE);
 
   glClearColor(0.2f, 0.3f, 0.4f, 1.0f);
-
-  const int gridH = 5;
-  const int gridW = 6;
     
-  Tile** grid = malloc(sizeof(Tile*) * gridH);
+  Tile** grid = malloc(sizeof(Tile*) * (gridH + balkonH));
 
   Mouse mouse = { .h = 0.005f, .w = 0.005f};
   
   // init grid
   {
-    for(int z=0;z<gridH;z++){
+    for(int z=0;z<gridH+balkonH;z++){
       grid[z] = calloc(gridW,sizeof(Tile));
+      const bool isBalkonZ = z == gridH+balkonH - 1;
+      
       for(int x=0;x<gridW;x++){
+	const int centerX = (gridW-1)/2;
+
+	if(isBalkonZ){
+	  if(x<centerX || x>centerX+2){
+	    continue;
+	  }
+	}
+	
 	if(z==0){
 	  grid[z][x].walls |= (1 << top); 
 	}
 
-	if(x==0){
+	if(x==0 || (isBalkonZ && x == centerX)){
 	  grid[z][x].walls |= (1 << left);
 	}
 
-	if(x==gridW-1){
+	if(x==gridW-1 || (isBalkonZ && x == centerX+2)){
 	  grid[z][x].walls |= (1 << right);
 	}
 
-	if(z==gridH-1){
+	if(z==gridH-1 || isBalkonZ){
 	  grid[z][x].walls |= (1 << bot);
+	  
+	  if(z==gridH-1 && x==(gridW-1)/2){
+	    grid[z][x].doors |= (1 << bot);
+	  }
 	}
-	
+
 	grid[z][x].center = 1;
       }
     }
@@ -126,12 +135,31 @@ int main(int argc, char* argv[]) {
 	      0.0,  0.0,  0.0,   /* where camera is pointing at */
 	      0.0,  1.0,  0.0);  /* which direction is up */
     glMatrixMode(GL_MODELVIEW);
+    
+    const float blockW =  0.1f;
+    const float blockD =  0.1f;
+    const float blockH =  0.025f;
 
-    for (int z = 0; z < gridH; z++) {
+    const float doorXPad =  blockW / 4;
+    
+    const float doorW =  blockW - doorXPad;
+
+    const float blockHForDoor = 0.2f;
+    //const float doorH =  blockH * 0.85;
+    const float doorH =  blockHForDoor * 0.85;
+
+    for (int z = 0; z < gridH + balkonH; z++) {
       for (int x = 0; x < gridW; x++) {
-	vec3 tile = { (float)x / 10 , 0, (float)z / 10 };
+	vec3 tile = { (double)x / 10 , 0, (double)z / 10 };
 
-	if (rayIntersectsTriangle(mouse.start, mouse.end, (vec3d) {tile.x,tile.y,tile.z})) {
+	if(!grid[z][x].center){
+	  continue;
+	}
+
+    const vec3 rt = { tile.x+0.1f, tile.y, tile.z + 0.1f };
+    const vec3 lb = tile;
+	
+    if (rayIntersectsTriangle(mouse.start, mouse.end, lb, rt)) {
 	  glBegin(GL_TRIANGLES);
 	  
 	  glColor3d(darkPurple);
@@ -147,21 +175,127 @@ int main(int argc, char* argv[]) {
 	  glEnd();
 	}
 
-	const float blockW = 0.1f;
-	const float blockD = 0.1f;
 
-	renderTile(tile, blockW, 0.1f, darkPurple);
-	//	renderCube(tile, 0.1f, blockW, 0.1f,darkPurple);
+    renderTile(tile, blockW, 0.1f, darkPurple);
 
 	for (int wallType = 0; wallType < sizeof(grid[z][x].walls); wallType++) {
+	  if ((grid[z][x].doors >> wallType) & 1) {
+
+	    // render door frame
+	    {
+	      glBegin(GL_LINE_LOOP);
+	  
+	      glColor3d(redColor);
+
+	      // first plank of frame(left)
+	      glVertex3d(tile.x, tile.y, tile.z + blockD);
+	      glVertex3d(tile.x + doorXPad/2, tile.y, tile.z + blockD);
+	      glVertex3d(tile.x, tile.y + doorH, tile.z + blockD);
+
+	      glVertex3d(tile.x, tile.y + doorH, tile.z + blockD);
+	      glVertex3d(tile.x + doorXPad/2, tile.y + doorH, tile.z + blockD);
+	      glVertex3d(tile.x + doorXPad/2, tile.y, tile.z + blockD);
+
+	      // top plank of frame
+	      
+	      glVertex3d(tile.x, tile.y + doorH, tile.z + blockD);
+	      glVertex3d(tile.x, tile.y + blockHForDoor, tile.z + blockD);
+	      glVertex3d(tile.x + blockW + doorXPad/2, tile.y + blockHForDoor, tile.z + blockD);
+
+	      glVertex3d(tile.x, tile.y + doorH, tile.z + blockD);
+	      glVertex3d(tile.x + blockW, tile.y + blockHForDoor, tile.z + blockD);
+	      glVertex3d(tile.x + blockW, tile.y + doorH, tile.z + blockD);
+
+	      // second plank of frame (right)
+
+	      glVertex3d(tile.x + doorW + doorXPad, tile.y + doorH, tile.z + blockD);
+	      glVertex3d(tile.x + doorW + doorXPad/2, tile.y, tile.z + blockD);
+	      glVertex3d(tile.x + doorW + doorXPad /2, tile.y + doorH, tile.z + blockD);
+
+	      glVertex3d(tile.x + doorW + doorXPad/2, tile.y, tile.z + blockD);
+	      glVertex3d(tile.x + doorW + doorXPad, tile.y, tile.z + blockD);
+	      glVertex3d(tile.x + doorW + doorXPad, tile.y + doorH, tile.z + blockD);
+	      
+	      glEnd();
+	    }
+
+	    vec3 doorPos = {tile.x + doorXPad / 2,tile.y,tile.z};
+
+	    vec3 lb = {0};
+	    vec3 rt = {0};
+
+	    switch(wallType){
+	    case(top):{
+	      lb = doorPos;
+	      rt = (vec3){tile.x+doorW, tile.y+doorH, tile.z};
+	      break;
+	    }
+	    case(bot):{
+	      lb = (vec3){tile.x,tile.y, tile.z + blockD};
+	      rt = (vec3){tile.x+doorW, tile.y+doorH, tile.z + blockD};
+	      break;
+	    }
+	    case(left):{
+	      lb = (vec3){tile.x,tile.y, tile.z};
+	      rt = (vec3){tile.x, tile.y+doorH, tile.z + blockD};
+	      break;
+	    }
+	    case(right):{
+	      lb = (vec3){tile.x + doorW,tile.y, tile.z};
+	      rt = (vec3){tile.x+ doorW, tile.y+doorH, tile.z + blockD};
+	      break;
+	    }
+	    default: break;
+	    }
+
+
+	    if(rayIntersectsTriangle(mouse.start,mouse.end,lb,rt)){
+	      renderWall(doorPos, GL_TRIANGLE_STRIP, doorW, blockD, wallType, doorH, blueColor);
+	    }else{
+	      renderWall(doorPos, GL_LINES, doorW, blockD, wallType, doorH, blueColor);
+	    };
+	    
+	    continue;
+	  }
+	    
+	  // wall rendering + wall selection
 	  if((grid[z][x].walls >> wallType) & 1){
-	    renderWall(tile, blockW, blockD, wallType, 0.1f,redColor);
+	    vec3 lb = {0};
+	    vec3 rt = {0};
+
+	    switch(wallType){
+	    case(top):{
+	      lb = tile;
+	      rt = (vec3){tile.x+blockW, tile.y+blockH, tile.z};
+	      break;
+	    }
+	    case(bot):{
+	      lb = (vec3){tile.x,tile.y, tile.z + blockD};
+	      rt = (vec3){tile.x+blockW, tile.y+blockH, tile.z + blockD};
+	      break;
+	    }
+	    case(left):{
+	      lb = (vec3){tile.x,tile.y, tile.z};
+	      rt = (vec3){tile.x, tile.y+blockH, tile.z + blockD};
+	      break;
+	    }
+	    case(right):{
+	      lb = (vec3){tile.x + blockW,tile.y, tile.z};
+	      rt = (vec3){tile.x+ blockW, tile.y+blockH, tile.z + blockD};
+	      break;
+	    }
+	    default: break;
+	    }
+
+	    if(rayIntersectsTriangle(mouse.start,mouse.end,lb,rt)){
+	      renderWall(tile, GL_TRIANGLE_STRIP, blockW, blockD, wallType, blockH,redColor);
+	    }else{
+	      renderWall(tile, GL_LINES, blockW, blockD, wallType, blockH,redColor);
+	    };
 	  }
 	}
       }
     }
-
-    printf("nest\n\n");
     
     renderCube(e1.pos, e1.w, e1.h, e1.d,greenColor);
 
@@ -204,8 +338,8 @@ int main(int argc, char* argv[]) {
       gluUnProject(winX, winY, 10.0, matModelView, matProjection, 
 		   viewport, &end.x, &end.y, &end.z);
 
-      mouse.start = start;
-      mouse.end = end;
+      mouse.start = vec3dToVec3(start);
+      mouse.end = vec3dToVec3(end);
       
       glBegin(GL_LINES);
       glVertex3d(start.x, start.y, start.z);
@@ -259,12 +393,12 @@ vec2 toIsoVec2(vec2 point){
   return (vec2){ point.x - point.y, (point.x + point.y)/2 };
 }
 
-void renderWall(vec3 pos, float blockW, float blockD, WallType wall, float wallH, float r, float g, float b){
+void renderWall(vec3 pos, GLenum mode , float blockW, float blockD, WallType wall, float wallH, float r, float g, float b){
   glPushMatrix();
   glTranslated(0.5, 0.5, 0.5);
   glColor3d(r, g, b);
 
-  glBegin(GL_LINES);
+  glBegin(mode);
 
   switch(wall){
   case(top):{
@@ -298,17 +432,18 @@ void renderWall(vec3 pos, float blockW, float blockD, WallType wall, float wallH
     break;
   }
   case(left):{
+    glVertex3d(pos.x,pos.y + wallH, pos.z);
     glVertex3d(pos.x, pos.y, pos.z);
-    glVertex3d(pos.x, pos.y + wallH, pos.z + blockD);
 
-    glVertex3d(pos.x,pos.y, pos.z);
+    //    glVertex3d(pos.x,pos.y, pos.z);
+    glVertex3d(pos.x, pos.y + wallH, pos.z + blockD);
     glVertex3d(pos.x,pos.y + wallH, pos.z);
 
-    glVertex3d(pos.x ,pos.y + wallH, pos.z);
-    glVertex3d(pos.x,pos.y + wallH, pos.z + blockD);
+        glVertex3d(pos.x ,pos.y + wallH, pos.z+ blockD);
+        glVertex3d(pos.x,pos.y, pos.z + blockD);
 
-    glVertex3d(pos.x ,pos.y, pos.z  + blockD);
-    glVertex3d(pos.x,pos.y + wallH, pos.z + blockD);
+        glVertex3d(pos.x,pos.y + wallH, pos.z + blockD);
+        glVertex3d(pos.x ,pos.y, pos.z);
   
     break;
   }
@@ -386,10 +521,10 @@ void renderCube(vec3 pos, float w, float h, float d, float r, float g, float b){
   glPopMatrix();
 }
 
-bool rayIntersectsTriangle(const vec3d start, const vec3d end, const vec3d point) {
-  vec3d dirfrac = {0};
+bool rayIntersectsTriangle(const vec3 start, const vec3 end, const vec3 lb, const vec3 rt) {
+  vec3 dirfrac = {0};
 
-  vec3d norm = normalize(end);
+  vec3 norm = normalize(end);
   
   dirfrac.x = 1.0f / norm.x;
   dirfrac.y = 1.0f / norm.y;
@@ -397,8 +532,8 @@ bool rayIntersectsTriangle(const vec3d start, const vec3d end, const vec3d point
   // lb is the corner of AABB with minimal coordinates - left bottom, rt is maximal corner
   // r.org is origin of ray
 
-  vec3d rt = { point.x+0.1f, point.y, point.z+0.1f  };
-  vec3d lb = { point.x, point.y, point.z };
+  //  vec3d rt = { point.x+0.1f, point.y, point.z+0.1f  };
+  //  vec3d lb = { point.x, point.y, point.z };
 
   float t1 = (lb.x - start.x)*dirfrac.x;
   float t2 = (rt.x - start.x)*dirfrac.x;
@@ -409,7 +544,6 @@ bool rayIntersectsTriangle(const vec3d start, const vec3d end, const vec3d point
 
   float tmin = max(max(min(t1, t2), min(t3, t4)), min(t5, t6));
   float tmax = min(min(max(t1, t2), max(t3, t4)), max(t5, t6));
-
   
   bool res = false;
   float t;
@@ -437,9 +571,9 @@ bool rayIntersectsTriangle(const vec3d start, const vec3d end, const vec3d point
   return res;
 }
 
-vec3d normalize(vec3d vec) {
+vec3 normalize(const vec3 vec) {
     float vecLen = sqrt(vec.x * vec.x + vec.y * vec.y + vec.z * vec.z);
-    vec3d norm = { 0 };
+    vec3 norm = { 0 };
 
     if (vecLen != 0.0f) {
         norm.x = vec.x / vecLen;
--- main.h
+++ main.h
@@ -16,13 +16,26 @@ typedef struct{
   float x,y,w,h;
 } rect2;
 
+typedef enum{
+  Room,
+  Balkon
+} LocationType;
+
 typedef struct {
   vec3 pos; // normal XY
   float w, h, d;
+
+  LocationType loc;
 } Entity;
 
 typedef struct{
   int walls;  // top, bot, left, right
+
+  // we can place doors and windows
+  // into wall but not
+  // simmonteniously
+  int doors;
+  int windows;
   
   int center;    
 } Tile;
@@ -34,28 +47,38 @@ typedef enum{
   left,
 } WallType;
 
-typedef struct{
-  vec3d worldPos;
+typedef enum{
+  door,
+} ObjectType;
 
-  vec3d start;
-  vec3d end;
+typedef struct{
+  int id;
+  vec3 pos;
+  ObjectType type;
+} Object;
+
+typedef struct{
+  vec3 start;
+  vec3 end;
 
   vec2 screenPos;
 
   float w, h; // of cursor
 } Mouse;
 
+#define vec3dToVec3(vec3d) (vec3){vec3d.x,vec3d.y,vec3d.z}
+
 vec2 toIsoVec2(vec2 point);
 
-void renderWall(vec3 pos, float blockW, float blockD, WallType wall, float wallH, float r, float g, float b);
+void renderWall(vec3 pos, GLenum mode, float blockW, float blockD, WallType wall, float wallH, float r, float g, float b);
 
 void renderCube(vec3 pos, float w, float h, float d, float r, float g, float b);
 
 void renderTile(vec3 pos, float w, float d, float r, float g, float b);
 
-vec3d normalize(vec3d vec);
+vec3 normalize(const vec3 vec);
 
-bool rayIntersectsTriangle(const vec3d start, const vec3d end, const vec3d point);
+bool rayIntersectsTriangle(const vec3 start, const vec3 end, const vec3 lb, const vec3 rt);
 
 #define FPS 60
 
@@ -66,3 +89,11 @@ bool rayIntersectsTriangle(const vec3d start, const vec3d end, const vec3d point
 #define blueColor 0.0f, 0.0f, 1.0f
 
 #define darkPurple 0.1f, 0.0f, 0.1f
+
+#define gridH 5
+#define gridW 6
+
+#define balkonH 1
+
+#define windowW 800
+#define windowH 600
